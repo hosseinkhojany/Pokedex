05a7807307d44c0289301866a7b43bc32ef57c7f
parisamahmoodi <parisamahmoodi70@gmail.com>
2020-11-30 00:25:28 +0330

    progress

--- app/src/main/java/com/example/pokedex/activitys/detail_fragment/detailprogress.kt
+++ app/src/main/java/com/example/pokedex/activitys/detail_fragment/detailprogress.kt
@@ -1,11 +1,14 @@
 package com.example.pokedex.activitys.detail_fragment
 
+import androidx.compose.animation.animate
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProgressIndicatorConstants
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,13 +21,23 @@ import com.example.pokedex.R
 
 @Composable
 fun Toprogress(modifier: Modifier = Modifier) {
+/*
+    var progress by remember {  mutableListOf(0.1f) }
+    val animatedProgress = animate(
+        target = progress,
+        animSpec = ProgressIndicatorConstants.DefaultProgressAnimationSpec
+    )
+*/
+
     Column(modifier = modifier) {
+
         Row(
             modifier = modifier,
             verticalAlignment = Alignment.CenterVertically
         ) {
 
             Text(
+                fontSize = 12.sp,
                 text = stringResource(id = R.string.HP),
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp)
@@ -49,6 +62,7 @@ fun Toprogress(modifier: Modifier = Modifier) {
         ) {
 
             Text(
+                fontSize = 12.sp,
                 text = stringResource(id = R.string.Attack),
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp)
@@ -73,6 +87,7 @@ fun Toprogress(modifier: Modifier = Modifier) {
         ) {
 
             Text(
+                fontSize = 12.sp,
                 text = stringResource(id = R.string.Defence),
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp)
@@ -96,6 +111,7 @@ fun Toprogress(modifier: Modifier = Modifier) {
         ) {
 
             Text(
+                fontSize = 12.sp,
                 text = stringResource(id = R.string.spAtk),
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp)
@@ -119,6 +135,7 @@ fun Toprogress(modifier: Modifier = Modifier) {
         ) {
 
             Text(
+                fontSize = 12.sp,
                 text = stringResource(id = R.string.spDef),
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp)
@@ -142,6 +159,7 @@ fun Toprogress(modifier: Modifier = Modifier) {
         ) {
 
             Text(
+                fontSize = 12.sp,
                 text = stringResource(id = R.string.Speed),
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp)
@@ -165,6 +183,7 @@ fun Toprogress(modifier: Modifier = Modifier) {
         ) {
 
             Text(
+                fontSize = 12.sp,
                 text = stringResource(id = R.string.Total),
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp)
